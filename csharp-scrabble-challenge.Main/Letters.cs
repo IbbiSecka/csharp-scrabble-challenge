f0ca541eb5f72fa1a22c11046f5e49db8e736390
ISecka <ISecka@N-NO-01-01-0093>
2024-08-22 17:22:55 +0200

    Started and set the functions properly, need some more fixing for the tests to pass

---
+++ csharp-scrabble-challenge.Main/Letters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_scrabble_challenge.Main
+{
+    public class Letters
+    {
+        public static Dictionary<char, int> getPair() { 
+            Dictionary<char,int> letters = new Dictionary<char, int>();
+            letters.Add('A', 1);
+            letters.Add('E', 1);
+            letters.Add('I', 1);
+            letters.Add('O', 1);
+            letters.Add('U', 1);
+            letters.Add('L', 1);
+            letters.Add('N', 1);
+            letters.Add('R', 1);
+            letters.Add('S', 1);
+            letters.Add('T', 1);
+
+            letters.Add('D', 2);
+            letters.Add('G', 2);
+
+            letters.Add('B', 3);
+            letters.Add('C', 3);
+            letters.Add('M', 3);
+            letters.Add('P', 3);
+
+            letters.Add('F', 4);
+            letters.Add('H', 4);
+            letters.Add('V', 4);
+            letters.Add('W', 4);
+            letters.Add('Y', 4);
+
+            letters.Add('K', 5);
+
+            letters.Add('J', 8);
+            letters.Add('X', 8);
+
+            letters.Add('Q', 10);
+            letters.Add('Z', 10);
+
+            return letters;
+
+        }
+
+
+    }
+}
--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -8,16 +8,33 @@ using System.Threading.Tasks;
 namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
+
     {
+        public string word;
+        public Dictionary<char, int> letterScore;
         public Scrabble(string word)
         {            
             //TODO: do something with the word variable
+            word = word.ToLower();
+            
+
         }
 
         public int score()
         {
+            int totalScore = 0;
             //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int score = 0;
+
+            var charArray = word.ToCharArray(); //Populate charArray with word split up
+
+            charArray.ToList().ForEach(x =>
+            {
+                int score = letterScore[Char.ToLower(x)];
+                totalScore = totalScore + score;
+            }); // Adding together each letter's score, populating it in the totalscore variable
+            Console.WriteLine(totalScore);
+            return totalScore;
         }
     }
 }
